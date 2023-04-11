bbaed6285f3a4d21159b4ebe122c31e1c68765f9
Charlie Örneskog <charlie.orneskog@gmail.com>
2023-04-11 15:36:54 +0200

    Storage done and also added buttons for complete/remove

--- todolist.js
+++ todolist.js
@@ -2,19 +2,56 @@ let tasks = [];
 const taskListElement = document.getElementById("task");
 const buttonElement = document.getElementById("sendButton");
 
+let arrayJSON;
+
 function addTaskToTaskList() {
   let inputValue = document.getElementById("textInput").value;
   tasks.push(inputValue);
-  console.log(tasks);
+  taskLoop();
+  saveToLocalStorage();
+}
 
+function saveToLocalStorage() {
+  arrayJSON = JSON.stringify(tasks);
+  localStorage.tasks = arrayJSON;
+}
+
+function taskLoop() {
+  taskListElement.innerHTML = "";
   for (let task of tasks) {
     const taskElement = document.createElement("div");
     taskElement.innerText = task;
     taskListElement.appendChild(taskElement);
+    taskElement.classList.add("completedTask");
+
+    const completeButton = document.createElement("span");
+    completeButton.innerText = "✔️";
+    taskElement.appendChild(completeButton);
+    completeButton.classList.add("buttonSpace");
+
+    const removeButton = document.createElement("span");
+    removeButton.innerText = "❌";
+    taskElement.appendChild(removeButton);
+    removeButton.classList.add("buttonSpace");
+
+    completeButton.addEventListener("click", () => {
+      taskElement.style = "background-color: #fff;";
+    });
+
+    const taskIndex = tasks.indexOf(task);
+    removeButton.addEventListener("click", () => {
+      tasks.splice(taskIndex, 1);
+      taskLoop();
+      saveToLocalStorage();
+    });
   }
 }
 
 const sendButton = document.getElementById("sendButton");
 sendButton.addEventListener("click", addTaskToTaskList);
 
-// Creating div's from the tasks array
+if (localStorage.tasks) {
+  tasks = JSON.parse(localStorage.tasks);
+}
+
+taskLoop();
